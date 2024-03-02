f6c56f164e946331cbd5385cdb85f7588829fa7b
Brendan <brendan@Brendans-MacBook-Pro.local>
2024-03-02 22:26:21 +1100

    Adding random number generation

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+#define a function to simulate rolling a dice
+def dice():
+    number=  [1,2,3,4,5,6]
+    return random.choice(number)
 
+series = np.array([dice() for x in range(10000)])
+print(series)
+
+plt.figure(figsize = (20,10))
+plt.hist(series,bins = 11,align = 'mid')
+plt.xlabel('Dice Number')
+plt.ylabel('Occurences')
+plt.grid()
+plt.show()
